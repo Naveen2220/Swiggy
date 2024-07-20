91929067297bb2e62d5a2294eb5fcbd8d77b1ea4
Naveen2220 <naveenboini001@gmail.com>
2024-07-20 23:48:38 +0530

    Update App.java

--- Swiggy/src/main/java/Naveen/App.java
+++ Swiggy/src/main/java/Naveen/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Naveen Kumar Boini!" );
+        System.out.println( "Hello Naveen Kumar Boini From  Sirclla !" );
     }
 }
